de3c0dd241f37ab9c9731684a318d2f0c8388c04
blackmanba <wen578351314@gmail.com>
2020-09-23 15:52:18 +0800

    test controller

---
+++ src/main/java/com/coda/demo/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.coda.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName TestController
+ * @Description TODO
+ * @Author wen578351314@gmail.com
+ * @Date 2020-09-23 15:51
+ * @Version 1.0
+ **/
+@RestController
+public class TestController {
+
+	@RequestMapping("/test")
+	public String test() {
+		return "Hello World";
+	}
+}
